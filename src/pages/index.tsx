00f3dc8edc33016bfa00119404a601303a6c7f44
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2022-03-26 17:33:39 -0300

    Miscellaneous changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,31 @@ import Image from "next/image";
 import { GoSearch } from "react-icons/go";
 
 import { Title, Form, Card, ContainerCard } from "@/styles/pages/Home";
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import api from "@/api";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+interface IPokemon {
+  name: string;
+  url: string;
+}
+
+interface IPokemonsList {
+  results: IPokemon[];
+}
+
+export default function Home({ results }: IPokemonsList) {
   const [search, setSearch] = useState("");
+
+  async function handleAddPokemon(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    try {
+    } catch {
+      alert("Ops, houve um erro ao buscar o pokemon.");
+    }
+  }
 
   return (
     <>
@@ -17,7 +38,7 @@ export default function Home() {
       />
       <Title>What Pokemon are looking for?</Title>
 
-      <Form>
+      <Form onSubmit={handleAddPokemon}>
         <input
           type="text"
           value={search}
@@ -30,55 +51,44 @@ export default function Home() {
       </Form>
 
       <ContainerCard>
-        <Card>
-          <img src="/assets/images/01.png" alt="" />
-          <div>
-            <h3>Bulbasaur</h3>
-            <div className="badge">
-              <img src="/assets/images/grass.png" alt="" />
-              <img src="/assets/images/poison.png" alt="" />
-            </div>
-          </div>
-          <span>#001</span>
-        </Card>
-
-        <Card>
-          <img src="/assets/images/01.png" alt="" />
-          <div>
-            <h3>Bulbasaur</h3>
-            <div className="badge">
-              <img src="/assets/images/grass.png" alt="" />
-              <img src="/assets/images/poison.png" alt="" />
-            </div>
-          </div>
-          <span>#002</span>
-        </Card>
-
-        <Card>
-          <img src="/assets/images/01.png" alt="" />
-          <div>
-            <h3>Bulbasaur</h3>
-            <div className="badge">
-              <img src="/assets/images/grass.png" alt="" />
-              <img src="/assets/images/poison.png" alt="" />
-            </div>
-          </div>
-          <span>#003</span>
-        </Card>
-
-        <Card>
-          <img src="/assets/images/01.png" alt="" />
-          <div>
-            <h3>Bulbasaur</h3>
-            <div className="badge">
-              <img src="/assets/images/grass.png" alt="" />
-              <img src="/assets/images/poison.png" alt="" />
-            </div>
-            <span>#003</span>
-          </div>
-          <span>#004</span>
-        </Card>
+        {results.map((pokemon) => {
+          return (
+            <Card>
+              <img src="/assets/images/01.png" alt="" />
+              <div>
+                <h3>{pokemon.name}</h3>
+                <div className="badge">
+                  <img src="/assets/images/grass.png" alt="" />
+                  <img src="/assets/images/poison.png" alt="" />
+                </div>
+              </div>
+              <span>#001</span>
+            </Card>
+          );
+        })}
       </ContainerCard>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<IPokemonsList> = async () => {
+  const pokemonlist: IPokemon[] = [];
+  const response = await api.get<IPokemonsList>(`pokemon`);
+
+  pokemonlist response.data.results.forEach(async function (pokemon) {
+    await fetch(pokemon.url)
+      .then((response) => response.json())
+      .then(function (pokeData) {
+        // console.log(pokeData);
+        pokemonlist.push(pokeData);
+      });
+  });
+
+  console.log(pokemonlist);
+
+  return {
+    props: {
+      results: pokemonlist,
+    },
+  };
+};
